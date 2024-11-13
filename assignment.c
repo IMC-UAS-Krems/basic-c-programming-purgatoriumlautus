dda809f07fcf164cf06d67b0a2034cbe5c81c7f7
pseudocodder <znaharura1@gmail.com>
2024-11-13 15:43:43 +0100

    initial input checks

--- assignment.c
+++ assignment.c
@@ -12,7 +12,18 @@ int main(int argc, char *argv[]) {
     int maxrand = 100;
 
     // WRITE YOUR CODE HERE
-    
+    if (argc != 3) {
+        printf("Incorrect usage. You provided %d arguments. The correct number of arguments is 2\n",argc-1);
+        return -1;
+    }
+    int x = atoi(argv[1]); //will return 0 for non-integer input
+    int y = atoi(argv[2]);
+    if (x > 0 && y > 0){
+        printf("x is %d\ny is %d\n",x,y);
+    }
+    else{
+        printf("Incorrect usage. The parameters you provided are not positive integers\n");
+    }
 
     return 0;
 }
